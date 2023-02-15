a3c7687974fceb3ed9a60ada782b8be67e83aa6c
arsenalkingsley <stepupitsreal@gmail.com>
2023-02-15 16:02:01 +0000

    initial commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+--LISTS ALL PRIVILEGES OF THE MYSQL USERS _0ID_1 AND USER _0D_2--
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+THIS THE README FILE FOR THE SQL_more_queries_project
