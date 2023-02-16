66e1c13486e8b7be1cae5e2807464fb453dcefda
arsenalkingsley <stepupitsreal@gmail.com>
2023-02-16 13:40:34 +0000

    final commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
---LISTS ALL PRIVILEGES OF THE MYSQL USERS _0ID_1 AND USER _0D_2--
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
